d0e06bc20fc90a395a6415c3ac9f83820d48eaa9
Dongfang334425 <Dongfang334425@163.com>
2020-11-15 22:04:31 +0800

    inherit Operation class and split different calculation classes

--- SimpleFactoryPattern/main.cpp
+++ SimpleFactoryPattern/main.cpp
@@ -6,30 +6,62 @@ using namespace::std;
 class COperation
 {
 public:
-	double GetResult(double numberA, double numberB, char operatorC)
+	virtual double GetResult(double numberA, double numberB, char operatorC) = 0;
+};
+
+class COperationAdd:public COperation
+{
+public:
+	double GetResult(double numberA, double numberB, char operatorC) override
 	{
+		
 		double res = 0.0f;
-		switch (operatorC)
-		{
-		case '+':
-			res = numberA + numberB;
-			break;
-		case '-':
-			res = numberA - numberB;
-			break;
-		case '*':
-			res = numberA * numberB;
-			break;
-		case '/':
-			res = numberA / numberB;
-			break;
-		}
+		res = numberA + numberB;
+		return res;
+	}
+	
+};
+
+class COperationSub :public COperation
+{
+public:
+	double GetResult(double numberA, double numberB, char operatorC) override
+	{
+
+		double res = 0.0f;
+		res = numberA - numberB;
 		return res;
 	}
 };
+
+class COperationMul :public COperation
+{
+public:
+	double GetResult(double numberA, double numberB, char operatorC) override
+	{
+
+		double res = 0.0f;
+		res = numberA * numberB;
+		return res;
+	}
+};
+
+class COperationDiv :public COperation
+{
+public:
+	double GetResult(double numberA, double numberB, char operatorC) override
+	{
+
+		double res = 0.0f;
+		res = numberA / numberB;
+		return res;
+	}
+};
+
 int main()
 {
 	//split calculation process and display process.
+	//inherit Operation class and split different calculation classes.
 	double a = 0.0f, b = 0.0f;
 	char c;
 	cout << "please enter number A:";
@@ -41,8 +73,29 @@ int main()
 	cout << "please enter number B:";
 	cin >> b;
 	cout << endl;
-	COperation myO;
-	cout << "result: " << myO.GetResult(a, b, c) << endl;
+	COperationAdd myOAdd;
+	COperationSub myOSub;
+	COperationMul myOMul;
+	COperationDiv myODiv;
+
+	COperationAdd* pmyOAdd = new COperationAdd(2);
+	switch (c)
+	{
+	case '+':
+		/*cout << pmyOAdd->GetResult(a, b, c) << endl;
+		*/
+		cout << myOAdd.GetResult(a,b,c) << endl;
+		break;
+	case '-':
+		cout << myOSub.GetResult(a, b, c) << endl;
+		break;
+	case '*':
+		cout << myOMul.GetResult(a, b, c) << endl;
+		break;
+	case '/':
+		cout << myODiv.GetResult(a, b, c) << endl;
+		break;
+	}
 	
 
 	return system("pause");
