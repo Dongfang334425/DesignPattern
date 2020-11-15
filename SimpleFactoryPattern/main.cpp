5953fd7dcf291a69f7ce0a3564001c0066972df1
Dongfang334425 <Dongfang334425@163.com>
2020-11-15 20:26:53 +0800

    split calculation process and display process

--- SimpleFactoryPattern/main.cpp
+++ SimpleFactoryPattern/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+
+
 using namespace::std;
+
+class COperation
+{
+public:
+	double GetResult(double numberA, double numberB, char operatorC)
+	{
+		double res = 0.0f;
+		switch (operatorC)
+		{
+		case '+':
+			res = numberA + numberB;
+			break;
+		case '-':
+			res = numberA - numberB;
+			break;
+		case '*':
+			res = numberA * numberB;
+			break;
+		case '/':
+			res = numberA / numberB;
+			break;
+		}
+		return res;
+	}
+};
 int main()
 {
-	//for beginner to implement a simple calculator.
-	int a = 0, b = 0;
+	//split calculation process and display process.
+	double a = 0.0f, b = 0.0f;
 	char c;
 	cout << "please enter number A:";
 	cin >> a;
@@ -14,23 +41,8 @@ int main()
 	cout << "please enter number B:";
 	cin >> b;
 	cout << endl;
-	//cout << a << " " << c << " " << b << endl;
-	cout << "result: ";
-	switch (c)
-	{
-	case '+':
-		cout << a + b << endl;
-		break;
-	case '-':
-		cout << a - b << endl;
-		break;
-	case '*':
-		cout << a * b << endl;
-		break;
-	case '/':
-		cout << a / b << endl;
-		break;
-	}
+	COperation myO;
+	cout << "result: " << myO.GetResult(a, b, c) << endl;
 	
 
 	return system("pause");
