1ca95d04e00abefc4255e89b5af142a0f431d6e4
Dongfang334425 <Dongfang334425@163.com>
2020-11-25 22:07:51 +0800

    3 students to help the aged man

--- FactoryPattern/main.cpp
+++ FactoryPattern/main.cpp
@@ -23,11 +23,17 @@ class Undergraduate :public LeiFeng
 };
 int main()
 {
-	LeiFeng* xueleifeng = new Undergraduate();
-	xueleifeng->BuyRice();
-	xueleifeng->Sweep();
-	xueleifeng->Wash();
-	delete xueleifeng;
-	xueleifeng = NULL;
+	LeiFeng* student1 = new Undergraduate();
+	student1->BuyRice();
+	LeiFeng* student2 = new Undergraduate();
+	student2->Sweep();
+	LeiFeng* student3 = new Undergraduate();
+	student3->Wash();
+	delete student1;
+	delete student2;
+	delete student3;
+	student1 = NULL;
+	student2 = NULL;
+	student3 = NULL;
 	return system("pause");
 }
