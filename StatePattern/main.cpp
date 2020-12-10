785a43f962163f8842ec35508736992add47b04a
Dongfang334425 <Dongfang334425@163.com>
2020-12-10 22:49:21 +0800

    state pattern: set state, transfer state

--- StatePattern/main.cpp
+++ StatePattern/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace::std;
 
+class State;
 class Work
 {
 private:
+	State* current;
 	int hour;
 	bool finish;
 	//Work(int _hour,bool _finish):hour(_hour),finish(_finish){}
 public:
-	Work():hour(0),finish(false){}
+	Work();
 
 	int getHour() { return hour; }
 	void setHour(int _hour) { hour = _hour; }
@@ -16,41 +18,76 @@ public:
 	bool getFinish() { return finish; }
 	void setFinish(bool _finish) { finish = _finish; }
 
-	void writeProgram()
-	{
-		if (hour < 12)
-		{
-			cout << "current time: " << hour << " morning." << endl;
-		}
-		else if (hour < 13)
-		{
-			cout << "current time: " << hour << " lunch time. Break" << endl;
-		}
-		else if (hour < 17)
-		{
-			cout << "current time: " << hour << " afternoon, go ahead" << endl;
-		}
-		else
-		{
-			if (finish)
-			{
-				cout << "current time: " << hour << "go off work" << endl;
-			}
-			else
-			{
-				if (hour < 21)
-				{
-					cout << "current time: " << hour << " work overtime, tired" << endl;
-				}
-				else
-				{
-					cout << "current time: " << hour << " fall asleep" << endl;
-				}
-			}
-		}
-	}
+	void writeProgram();
 
+	void setState(State* s)
+	{
+		current = s;
+	}
 };
+
+class State
+{
+public:
+	virtual void WriteProgram(Work* w)=0;
+};
+
+class ForenoonState :public State
+{
+	void WriteProgram(Work* w) override;
+};
+
+class NoonState :public State
+{
+	void WriteProgram(Work* w) override;
+};
+
+class AfternoonState :public State
+{
+	void WriteProgram(Work* w) override;
+};
+
+Work::Work():hour(0), finish(false)
+{
+	current = new ForenoonState();
+}
+
+void Work::writeProgram()
+{
+	current->WriteProgram(this);
+}
+
+void ForenoonState::WriteProgram(Work* w)
+{
+	if (w->getHour() < 12)
+	{
+		cout << "current time: " << w->getHour() << " morning." << endl;
+	}
+	else
+	{
+		w->setState(new NoonState());
+		w->writeProgram();
+	}
+}
+
+void NoonState::WriteProgram(Work* w)
+{
+	if (w->getHour() < 13)
+	{
+		cout << "current time: " << w->getHour() << " lunch time. Break" << endl;
+	}
+	else
+	{
+		w->setState(new AfternoonState());
+		w->writeProgram();
+	}
+	
+}
+
+void AfternoonState::WriteProgram(Work* w)
+{
+	cout << "current time: " << w->getHour() << " afternoon, go ahead" << endl;
+}
 
 int main()
 {
@@ -65,7 +102,7 @@ int main()
 	emergencyProjects->writeProgram();
 	emergencyProjects->setHour(14);
 	emergencyProjects->writeProgram();
-	emergencyProjects->setHour(17);
+	/*emergencyProjects->setHour(17);
 	
 
 	emergencyProjects->setFinish(false);
@@ -73,7 +110,7 @@ int main()
 	emergencyProjects->setHour(19);
 	emergencyProjects->writeProgram();
 	emergencyProjects->setHour(22);
-	emergencyProjects->writeProgram();
+	emergencyProjects->writeProgram();*/
 
 	return system("pause");
 }
